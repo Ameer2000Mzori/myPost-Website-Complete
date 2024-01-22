0c655521e469e08cf2057f1ba8514f1bda8fd08e
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-22 16:14:14 +0100

    installed and coneected dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,15 @@
 // need to distribute the server to people
 
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   console.log("hallo world");
 });
 
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
