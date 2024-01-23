20d3526ccbf482d523af56e6984463b2f60ebcb8
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-23 16:52:49 +0100

    fetched self created api, and small bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [posts, SetPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/all/posts");
+      const data = await response.json();
+      console.log(data);
+      SetPosts(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Video List</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <p key={index}>{post.title}</p>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
